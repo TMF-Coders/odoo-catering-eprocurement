c38bd8bb75074479c345224d6d5b4742639dce22
Ruben Morgade <rmorgade@tmfcoders.com>
2026-03-30 10:32:22 +0200

    style: add Odoo App Store visual assets (icon, screenshot, index.html)

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,7 @@
     'name': 'Catering e-Procurement Kiosk',
     'version': '19.0.1.0.0',
     'category': 'Inventory/Purchase',
+    'images': ['static/description/main_screenshot.png'],
     'summary': 'Touch-friendly B2B purchasing kiosk for kitchen staff and chefs',
     'description': """
 Vituallas HORECA Suite: Catering e-Procurement
